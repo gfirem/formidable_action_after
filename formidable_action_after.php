35f9c3a3d5e65a3d6b51a49606a22762fe04f432
gfirem@gmail.com <gfirem@gmail.com>
2017-03-06 12:03:42 -0400

    Updating freemius

--- formidable_action_after.php
+++ formidable_action_after.php
@@ -78,24 +78,23 @@ if ( ! class_exists( 'formidable_action_after' ) ) {
 				require_once FAA_CLASSES_PATH . '/include/freemius/start.php';
 				
 				$act_fs = fs_dynamic_init( array(
-					'id'                  => '838',
-					'slug'                => 'formidable_action_after',
-					'type'                => 'plugin',
-					'public_key'          => 'pk_2d9d898b7a365dd4d7fabd1e72b85',
-					'is_premium'          => true,
-					'is_premium_only'     => true,
-					'has_premium_version' => false,
-					'has_addons'          => false,
-					'has_paid_plans'      => true,
-					'is_org_compliant'    => false,
-					'menu'                => array(
+					'id'               => '838',
+					'slug'             => 'formidable_action_after',
+					'type'             => 'plugin',
+					'public_key'       => 'pk_2d9d898b7a365dd4d7fabd1e72b85',
+					'is_premium'       => true,
+					'is_premium_only'  => true,
+					'has_addons'       => false,
+					'has_paid_plans'   => true,
+					'is_org_compliant' => false,
+					'menu'             => array(
 						'slug'       => 'formidable_action_after',
 						'first-path' => 'admin.php?page=formidable_action_after',
 						'support'    => false,
 					),
 					// Set the SDK to work in a sandbox mode (for development & testing).
 					// IMPORTANT: MAKE SURE TO REMOVE SECRET KEY BEFORE DEPLOYMENT.
-					'secret_key'          => 'sk_6l4wmhM2&-XuD8e66;Kl+yir@:=<U',
+					'secret_key'       => 'sk_6l4wmhM2&-XuD8e66;Kl+yir@:=<U',
 				) );
 			}
 			
